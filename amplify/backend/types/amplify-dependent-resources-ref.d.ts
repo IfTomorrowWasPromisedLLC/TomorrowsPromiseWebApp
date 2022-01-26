a641b1c8195432b2345e75881581deba6b5e98d6
CWashington98 <43585651+CWashington98@users.noreply.github.com>
2022-01-26 18:41:43 -0500

    Initialize project using Create React App

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,28 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "TomorrowsPromiseWebA": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string",
+            "CreatedSNSRole": "string"
+        }
+    },
+    "function": {
+        "S3Triggerd0ee215e": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        }
+    },
+    "storage": {
+        "test": {
+            "BucketName": "string",
+            "Region": "string"
+        }
+    }
+}
