2f2a19586237510e110423ac1fe0d1a767257c39
CWashington98 <43585651+CWashington98@users.noreply.github.com>
2022-03-12 22:48:07 -0500

    working on beneficiary api/database

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,29 +1,13 @@
 export type AmplifyDependentResourcesAttributes = {
     "auth": {
-        "tomorrowspromiseweba60965faf": {
+        "itwpwebapp39cedce5": {
             "IdentityPoolId": "string",
             "IdentityPoolName": "string",
-            "HostedUIDomain": "string",
-            "OAuthMetadata": "string",
             "UserPoolId": "string",
             "UserPoolArn": "string",
             "UserPoolName": "string",
             "AppClientIDWeb": "string",
             "AppClientID": "string"
-        }
-    },
-    "function": {
-        "S3Triggerb26b4f5c": {
-            "Name": "string",
-            "Arn": "string",
-            "Region": "string",
-            "LambdaExecutionRole": "string"
-        }
-    },
-    "storage": {
-        "itwpDevTest": {
-            "BucketName": "string",
-            "Region": "string"
         }
     }
 }
